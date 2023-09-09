e8ea2fd2fcc1a8eee906518238bece617ee347b9
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-09-09 19:12:55 -0300

    Creating forms

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import styles from "./Form.module.css";
 import { Step1 } from "../Step1/Step1";
 
 export const Form = () =>{
 
+const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: ""
+      });
+
+const [step, setStep] = useState(1);
+
+const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
 
     return(
         <div className={styles.container}>
--- src/components/Step1/Step1.tsx
+++ src/components/Step1/Step1.tsx
@@ -5,7 +5,19 @@ export const Step1 = () => {
 
     return(
         <div className={styles.container}>
-            
+            <div className={styles.info}>
+                <h2 className={styles.title}>Personal info</h2>
+                <p className={styles.titleDesc}>Please provide your name, email address and phone number.</p>
+            </div>
+            <div className={styles.divInputs}>
+                <div className={styles.input}>
+                    <label>Name</label>
+                    <input 
+                    type="text"
+                    placeholder="e.g. Stephen King" 
+                    />
+                </div>
+            </div>
         </div>
     )
 }
