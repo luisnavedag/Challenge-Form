1d40d3a87bfad303ec6d07ba647e829c5ef8afb7
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-09-08 21:30:59 -0300

    Starting project

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,11 @@
+import styles from "./Form.module.css"
+
+export const Form = () =>{
+
+
+    return(
+        <div className={styles.container}>
+            <h1>Soy el form</h1>
+        </div>
+    )
+}
