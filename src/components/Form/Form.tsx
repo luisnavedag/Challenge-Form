53df98d8c4d134f9dbbc68e7a3a479e71091e877
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-09-09 22:05:22 -0300

    Next steps

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import styles from "./Form.module.css";
 import { Step1 } from "../Step1/Step1";
+import { Step2 } from "../Step2/Step2";
+import { Step3 } from "../Step3/Step3";
 
-export const Form = () =>{
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+}
 
-const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: ""
-      });
+export const Form = () => {
 
-const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    phone: ""
+  });
 
-const nextStep = () => {
+  const [step, setStep] = useState<number>(1);
+
+  const nextStep = () => {
     setStep(step + 1);
   };
 
@@ -20,14 +28,27 @@ const nextStep = () => {
     setStep(step - 1);
   };
 
-    return(
-        <div className={styles.container}>
-            <div className={styles.sideBar}>
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <Step1 formData={formData} setFormData={setFormData} nextStep={nextStep} />;
+      case 2:
+        return <Step2 formData={formData} setFormData={setFormData} nextStep={nextStep} prevStep={prevStep} />;
+      case 3:
+        return <Step3 formData={formData} setFormData={setFormData} prevStep={prevStep} />;
+      default:
+        return null;
+    }
+  };
 
-            </div>
-            <div className={styles.divForm}>
-                <Step1 />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+
+      </div>
+      <div className={styles.divForm}>
+        {renderStep()}
+      </div>
+    </div>
+  )
 }
--- src/components/Step1/Step1.tsx
+++ src/components/Step1/Step1.tsx
@@ -1,9 +1,23 @@
-import styles from "./Step1.module.css";
+import styles from "./Step1.module.css"
 
-export const Step1 = () => {
+interface Step1Props {
+    formData: {
+      name: string;
+      email: string;
+      phone: string;
+    };
+    setFormData: React.Dispatch<React.SetStateAction<{
+      name: string;
+      email: string;
+      phone: string;
+    }>>;
+    nextStep: () => void;
+  }
+
+export const Step1: React.FC<Step1Props> = ({ formData, setFormData, nextStep }) => {
 
 
-    return(
+    return (
         <div className={styles.container}>
             <div className={styles.info}>
                 <h2 className={styles.title}>Personal info</h2>
@@ -11,12 +25,35 @@ export const Step1 = () => {
             </div>
             <div className={styles.divInputs}>
                 <div className={styles.input}>
-                    <label>Name</label>
-                    <input 
-                    type="text"
-                    placeholder="e.g. Stephen King" 
+                    <label className="block text-gray-600 text-sm font-bold mb-2">Name</label>
+                    <input
+                        type="text"
+                        placeholder="e.g. Stephen King"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        value={formData.name}
                     />
                 </div>
+                <div className={styles.input}>
+                    <label className="block text-gray-600 text-sm font-bold mb-2">Email Address</label>
+                    <input
+                        type="text"
+                        placeholder="e.g. stephenking@lorem.com"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        value={formData.email}
+                    />
+                </div>
+                <div className={styles.input}>
+                    <label className="block text-gray-600 text-sm font-bold mb-2">Phone Number</label>
+                    <input
+                        type="text"
+                        placeholder="e.g. +1 234 567 890"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        value={formData.phone}
+                    />
+                </div>
+            </div>
+            <div className={styles.divButtons}>
+                <button className={styles.nextButton}>Next Step</button>
             </div>
         </div>
     )
