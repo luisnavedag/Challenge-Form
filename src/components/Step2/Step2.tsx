94293172062ea8c1521c275df914bcea2097a6ad
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-09-10 14:33:59 -0300

    Add switch toggle for change selectedPlan

--- src/components/Step2/Step2.tsx
+++ src/components/Step2/Step2.tsx
@@ -48,23 +48,46 @@ export const Step2: React.FC<Step2Props> = ({ formData, setFormData, nextStep, p
             </div>
             <div className={styles.divPlans}>
                 <div className={styles.plan} onClick={() => handlePlanSelect("Arcade")}>
-                    <img src={arcade} alt="arcade" width={40} height={40}/>
-                    <h3>Arcade</h3>
-                    <p>Price: {formData.subscriptionType === "Monthly" ? "$10/month" : "$100/year"}</p>
+                    <img src={arcade} alt="arcade" width={30} height={30} />
+                    <div className={styles.planInfo}>
+                        <h3>Arcade</h3>
+                        <p>{formData.subscriptionType === "Monthly" ? "$9/mo" : "$90/yr"}</p>
+                    </div>
                 </div>
                 <div className={styles.plan}>
-                    <img src={advanced} alt="advanced" width={40} height={40}/>
-                    <h3>Advanced</h3>
-                    <p>Price: {formData.subscriptionType === "Monthly" ? "$20/month" : "$200/year"}</p>
+                    <img src={advanced} alt="advanced" width={30} height={30} />
+                    <div className={styles.planInfo}>
+                        <h3>Advanced</h3>
+                        <p>{formData.subscriptionType === "Monthly" ? "$12/mo" : "$120/yr"}</p>
+                    </div>
                 </div>
                 <div className={styles.plan}>
-                    <img src={pro} alt="pro" width={40} height={40}/>
-                    <h3>Pro</h3>
-                    <p>Price: {formData.subscriptionType === "Monthly" ? "$30/month" : "$300/year"}</p>
+                    <img src={pro} alt="pro" width={30} height={30} />
+                    <div className={styles.planInfo}>
+                        <h3>Pro</h3>
+                        <p>{formData.subscriptionType === "Monthly" ? "$15/mo" : "$150/yr"}</p>
+                    </div>
                 </div>
             </div>
             <div className={styles.divToggle}>
-
+            <label className="flex items-center space-x-2">
+          <span className={formData.subscriptionType === "Monthly" ? "text-blue-900" : "text-slate-400"}>Monthly</span>
+          <div className="relative">
+            <input
+              type="checkbox"
+              className="sr-only"
+              checked={formData.subscriptionType === "Annual"}
+              onChange={handleToggleSubscription}
+            />
+            <div className="w-14 h-8 bg-blue-950 rounded-full shadow-inner cursor-pointer"></div>
+            <div
+              className={`absolute w-6 h-6 bg-white rounded-full transition-transform transform ${
+                formData.subscriptionType === "Annual" ? "translate-x-6" : ""
+              }`}
+            ></div>
+          </div>
+          <span className={formData.subscriptionType === "Annual" ? "text-blue-900" : "text-slate-400"}>Yearly</span>
+        </label>
             </div>
             <div className={styles.divButtons}>
                 <p className={styles.back} onClick={prevStep}>Go Back</p>
